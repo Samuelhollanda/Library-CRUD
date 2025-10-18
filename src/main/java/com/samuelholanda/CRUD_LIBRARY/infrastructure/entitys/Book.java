62133847ee853d3e4432430b5d5dba0654f6f211
Samuel Holanda <samueloliveirad337@gmail.com>
2025-10-18 16:45:57 -0300

    Adicao da entidade livro mais criacao do repositorio de livros

--- src/main/java/com/samuelholanda/CRUD_LIBRARY/infrastructure/entitys/Book.java
+++ src/main/java/com/samuelholanda/CRUD_LIBRARY/infrastructure/entitys/Book.java
@@ -1,4 +1,25 @@
 package com.samuelholanda.CRUD_LIBRARY.infrastructure.entitys;
 
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "books")
+@Entity
 public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    @Column(name = "author")
+    private String author;
+
+    @Column(name = "publisher")
+    private String publisher;
 }
--- src/main/java/com/samuelholanda/CRUD_LIBRARY/infrastructure/repository/BookRepository.java
+++ src/main/java/com/samuelholanda/CRUD_LIBRARY/infrastructure/repository/BookRepository.java
@@ -1,4 +1,15 @@
 package com.samuelholanda.CRUD_LIBRARY.infrastructure.repository;
 
-public interface BookRepository {
+import com.samuelholanda.CRUD_LIBRARY.infrastructure.entitys.Book;
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface BookRepository extends JpaRepository<Book, Integer> {
+    Optional<Book> getBookById(Integer id);
+
+    @Transactional
+    void deleteBookByName(String name);
+
 }
